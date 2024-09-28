88b0c9f7931bcf5b8b4797848801960693988b94
yonghun Jeon <jeon9412@gmail.com>
2024-09-29 02:19:24 +0900

    test6

--- public/js/random-restaurant.js
+++ public/js/random-restaurant.js
@@ -134,6 +134,10 @@ document.addEventListener('DOMContentLoaded', function() {
             <a href="${selectedRestaurant.place_url}" target="_blank" class="restaurant-link">자세히 보기</a>
         `;
 
+        // Add this line to change overflow to auto when restaurant info is displayed
+        document.documentElement.style.overflow = 'auto';
+        document.body.style.overflow = 'auto';
+
         const orangeMarkerSVG = `
             <svg width="24" height="24" viewBox="0 0 24 24" fill="#FFA500" xmlns="http://www.w3.org/2000/svg">
                 <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5a2.5 2.5 0 1 1 0-5 2.5 2.5 0 0 1 0 5z"/>
